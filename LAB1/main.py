cc53ddc659be1651148601d26c27c53afa1714e1
DYusL <dyl9293@gmail.com>
2017-03-30 17:43:02 +0200

    Examples and function and enframe function
    Examples and function and enframe function

--- LAB1/main.py
+++ LAB1/main.py
@@ -1,5 +1,7 @@
 import numpy as np
+import proto
 
-example = np.load('example.npz')['example'].item()
-tidigits = np.load('tidigits.npz')['tidigits']
-
+example = np.load('example_python3.npz')['example'].item()
+tidigits = np.load('tidigits_python3.npz')['tidigits']
+# print(example['samples'])
+frames = proto.enframe(example['samples'], int(16000*0.02), int(16000*0.01))
